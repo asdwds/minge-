de76d1e9df13991f9af791c5cfae12bd1b384c50
spi8823 <shin.8823@i.softbank.jp>
2015-05-26 22:58:05 +0900

    とりあえずテストできるとこまで早くがんばらないと

---
+++ minge-/Assets/Scripts/OfflineGameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using Game;
+
+namespace Game
+{
+    public class OfflineGameManager : MonoBehaviour
+    {
+        public PlayerController Player1;
+        public PlayerController Player2;
+
+        public void Awake()
+        {
+            this.Player1 = GameObject.Find("Player1").GetComponent<PlayerController>();
+            //this.Player2 = GameObject.Find("Player2").GetComponent<PlayerController>();
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+        }
+
+        public void StartStage()
+        {
+
+        }
+
+        public void AcceptInput()
+        {
+            this.Player1.Move(Input.GetAxis("Player1_Vertical1"), Input.GetAxis("Player1_Horizontal1"));
+            this.Player1.RotateCamera(Input.GetAxis("Player1_Vertical2"), Input.GetAxis("Player1_Horizontal2"));
+        }
+    }
+}
---
+++ minge-/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using Game;
+
+namespace Game
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private Transform tf;
+        private Transform cameratf;
+        private Transform modeltf;
+
+        public void Awake()
+        {
+            this.tf = this.transform;
+            this.cameratf = tf.FindChild("Camera");
+            this.modeltf = tf.FindChild("Model");
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Move(float vertical, float horizontal)
+        {
+        }
+
+        public void RotateCamera(float vertical, float horizontal)
+        {
+
+        }
+    }
+}
